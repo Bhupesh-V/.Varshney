068bee5ff88dc9bdf03a372a1aee8321de4fee91
Bhupesh-V <varshneybhupesh@gmail.com>
2020-11-04 20:35:41 +0530

    i am hungry and this is not working

--- scripts/contributors.py
+++ scripts/contributors.py
@@ -3,22 +3,20 @@
 # Find total contributors on all repositories of a github user (WIP)
 
 # TODO
-# 1. Add basic auth handlers
+# 1. Add basic auth handlers (Done)
 # 2. Generate & write reports to a file
 # 3. Handle pagination
 # 4. Clean up error handling
+# 5. Fetch User's Organisation
 
 import urllib.parse
 import urllib.request
-import json
+import json, sys
 
 HEADERS = {"User-Agent": "Mozilla/5.0"}
 
-# Add "?per_page=200" to find more contributors
-
 base_url = "https://api.github.com"
 repo_contributors_url = ""
-pull_request = "https://api.github.com/search/issues?q=author%3A{username}+is:pr&sort=created&order=asc"
 
 
 def request(url):
@@ -26,20 +24,39 @@ def request(url):
     with urllib.request.urlopen(req) as response:
         res = json.loads(response.read().decode("utf-8"))
         if response.headers["link"] is not None:
-            paginate = response.headers["link"].split(";")
+            #print(response.headers["link"].split(";"))
+            header_links = response.headers["link"].split(',')[0].split(';')
+            if header_links[1] == ' rel="next"':
+                print(header_links)
+                next_url = header_links[0]
+                next_url = next_url[1:len(next_url)]
         else:
-            paginate = None
+            next_url = None
 
-    return res, paginate
+    return res, next_url
 
 # with open('contributors.json', 'w') as file:
 # 	json.dump(repo_list, file)
+
+def handle_pagination(url):
+    res_list = []
+    res, next_url = request(url)
+    res_list += res
+    while next_url:
+        res, next_url = request(next_url)
+        if next_url is None:
+            break
+        else:
+            print("got new url")
+            res_list += res
+
+    return res_list
 
 
 def find_user_repos(user):
     repo_list = []
     try:
-        repos, paginate = request(
+        repos = handle_pagination(
             f"{base_url}/users/{user}/repos?type=owner&per_page=100"
         )
         for repo in repos:
@@ -60,14 +77,14 @@ def find_contributors(repo_list, user):
     total_contributors = 0
     for repo in repo_list:
         try:
-            contributors, paginate = request(
-                f"""{base_url}/repos/{user}/{repo["name"]}/contributors"""
+            contributors = handle_pagination(
+                f"""{base_url}/repos/{user}/{repo["name"]}/contributors?per_page=100"""
             )
             ccon = 0
             for con in contributors:
-                if con["login"] != user:
+                if con["login"] != user and con["type"] != "Bot":
                     ccon += 1
-            print(f"""Contributors in {repo["name"]}: {con}""")
+            print(f"""Contributors in {repo["name"]}: {ccon}""")
         except urllib.error.HTTPError as e:
             if e.code == 403:
                 print("GitHub API rate limit exceeded. Only 60 requests/hour")
@@ -75,7 +92,7 @@ def find_contributors(repo_list, user):
             else:
                 print(f"No contributors in {repo['name']}")
                 pass
-        total_contributors += con
+        total_contributors += ccon
 
     print(f"Total Contributors in {user}'s repos : {total_contributors}")
 
@@ -100,7 +117,16 @@ def find_first_contributors(contributors, user, user_repos):
 
 
 if __name__ == "__main__":
-    user = "Bhupesh-V"
+    # yeah i will use argparse later
+    if len(sys.argv) < 2:
+        print("Please provide Github username")
+        exit()
+    elif len(sys.argv) == 3:
+        HEADERS["Authorization"] = f"token {sys.argv[2]}"
+    else:
+        print("No token provided, requests will be limited by GitHub")
+
+    user = sys.argv[1]
     print(f"Fetching {user}'s repositories ...")
     repositories = find_user_repos(user)
     print(f"Fetching contributors ...")
