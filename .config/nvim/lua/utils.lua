00b918b06015de0c2da60fa18c987599c9e767d0
Bhupesh-V <varshneybhupesh@gmail.com>
2025-08-19 20:13:33 +0530

    fix: invalid string error on first time use of comment toggler

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -54,9 +54,16 @@ M.get_visual_selection = function()
 	local s_start = vim.fn.getpos("'<")
 	local s_end = vim.fn.getpos("'>")
 
+	if s_start[2] == 0 and s_end[2] == 0 then
+		-- fresh neovim boot, visual markers are empty
+		return {}, 0, 0
+	end
+
 	local n_lines = math.abs(s_end[2] - s_start[2]) + 1
 	local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+
 	lines[1] = string.sub(lines[1], s_start[3], -1)
+
 	if n_lines == 1 then
 		lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
 	else
